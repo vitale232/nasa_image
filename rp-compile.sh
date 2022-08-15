aa0b2fb29af32ce01c44948a29bfeb473145f9f0
Andrew Vitale <vitale232@gmail.com>
2022-08-15 14:02:22 -0400

    fix: scp release build

--- rp-compile.sh
+++ rp-compile.sh
@@ -4,6 +4,6 @@ TARGET=armv7-unknown-linux-gnueabihf
 
 cargo build --target $TARGET --release
 
-sshpass -f .pi_pass scp -r ./target/$TARGET/debug/nasa_image vitale232@$PI_IP:/home/vitale232/nasa/nasa_image
+sshpass -f .pi_pass scp -r ./target/$TARGET/release/nasa_image vitale232@$PI_IP:/home/vitale232/nasa/nasa_image
 sshpass -f .pi_pass scp -r ./.env vitale232@$PI_IP:/home/vitale232/nasa/.env
 
